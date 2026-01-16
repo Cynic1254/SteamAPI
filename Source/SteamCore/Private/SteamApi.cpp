8fe913772ba12b503f7fe5e43f09308aafe328ca
Cynic <lassewolv@gmail.com>
2026-01-16 08:09:24 +0100

    Change copyright notice and FilterPlugin.ini

--- Source/SteamCore/Private/SteamApi.cpp
+++ Source/SteamCore/Private/SteamApi.cpp
@@ -1,6 +1,4 @@
 ﻿// Copyright 2026 Cynic. All Rights Reserved.
-// Licensed under the Apache License, Version 2.0.
-// See LICENSE file in the project root for full license information.
 
 
 #include "SteamApi.h"
